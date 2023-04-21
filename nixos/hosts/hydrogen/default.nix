9ca6b5a3d4f270e39cbeae236da2f9246d8bae5c
Matthew McCune <matt@mattrick.org>
2023-04-21 16:47:38 -0700

    Update hostname

--- nixos/hosts/hydrogen/default.nix
+++ nixos/hosts/hydrogen/default.nix
@@ -9,6 +9,8 @@
     ./gdm.nix
   ];
 
+  networking.hostName = "hydrogen";
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
